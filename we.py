1bcf2489e7e5d082d9719be23dbe89940c9e3e11
hohoishi <wenchiehlu0613@gmai.com>
2024-08-28 17:25:53 +0800

    we done

---
+++ we.py
@@ -0,0 +1,48 @@
+from threading import Thread
+import requests
+import json
+
+class WeaG:
+
+    def __init__(self):
+        self.env = json.load(open('env.json'))
+        self.url = self.env['url']
+        self.AUTHENTICATION = self.env['token']
+
+    def grab(self , site):
+        '''this is a main funtion for grabbing data from CWA's open data api ->
+        {O:....}'''
+        def _grab(url, site ):
+        
+            params = {'Authorization':self.AUTHENTICATION,
+                            'StationName':site}
+            
+            r = requests.get(url, params=params)
+            if r.json()['records']['Station']:
+                s = r.json()['records']['Station'][0]
+                info['O'] = r.json()['records']['Station'][0]['ObsTime']['DateTime']
+                if 'WeatherElement' in s :
+
+                    info['T'] = float((r.json()['records']['Station'][0]['WeatherElement']['AirTemperature']))
+                    info['H'] = float(r.json()['records']['Station'][0]['WeatherElement']['RelativeHumidity'])/100
+                elif 'RainfallElement' in s :
+                    info['R'] = r.json()['records']['Station'][0]['RainfallElement']['Now']['Precipitation']
+        
+        info = {}
+        ths = [None] * len(self.url)
+        for i in range(len(self.url)):
+            ths[i] = Thread(target=_grab, args=(self.url[i],site), daemon=True)
+            ths[i].start()
+            ths[i].join()
+
+        return info
+if __name__ == '__main__':
+    import argparse 
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('site')
+    args = parser.parse_args()
+
+    w = WeaG() 
+    print(w.grab(args.site))
